dc8a5af94aca345356dddf1a2d0f3932a389a78c
jans <ubuntujans@gmail.com>
2024-05-26 16:35:04 -0400

    fazer a renderização da pagina home adicionando as imagens para o carrossel

---
+++ script.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const images = [
+        { src: "/imagens/jogos/Zelda Breath of the Wild.png" , alt: "The Legend of Zelda: Breath of the Wild", link: "https://zelda.nintendo.com/breath-of-the-wild/" },
+        { src: "/imagens/jogos/Palword.png" , alt: "Palword", link: "https://www.pocketpair.jp/palworld" },
+        { src: "/imagens/jogos/Sekiro Shadows Die Twice.png", alt: "Sekiro Shadows Die Twice", link: "https://www.sekirothegame.com/br/pt/" },
+        { src: "/imagens/jogos/Horizon Forbidden West.png", alt: "Horizon Forbidden West", link: "https://www.playstation.com/en-us/horizon/" },
+        { src: "/imagens/jogos/God of War.png", alt: "God of War", link: "https://www.playstation.com/en-us/god-of-war/" }
+    ];
+
+});
